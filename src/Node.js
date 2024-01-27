6613746053430c3f0d1071b4ab57935547a77732
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-01-27 10:48:35 +0300

    before lineto

--- src/Node.js
+++ src/Node.js
@@ -23,8 +23,7 @@ export default function Node(props) {
 
         </button>
 
-    )
-        ;
+    );
 
 
 }
--- src/NodeMap.js
+++ src/NodeMap.js
@@ -8,19 +8,18 @@ export default function NodeMap(props){
     return (
         <div className="NodeMap">
             <DndContext className="NodeMap" onDragStart={props.nodes.nodeClickHandler}>
-                TEST
+
                 {
 
                     props.nodes.layeredNodes().map(
-
                         (layer) => (<Layer layer={layer}/>)
-
-
                     )
 
 
                 }
-
+                <svg width="500" height="500">
+                    <path d="M100 40 Q100 100 100 100" stroke="black" fill="transparent"/>
+                </svg>
 
             </DndContext>
         </div>
