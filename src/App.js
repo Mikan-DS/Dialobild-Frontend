15b30d9e1dd95318a8029fddc7a28b7fcc3a8a81
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-02-01 08:20:22 +0300

    clear all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React, {useState} from 'react';
 import './App.css';
-import DnDExample from "./DnDExample";
-import DialobildCanvas from "./DialobildCanvas";
-import useNodes from "./useNodes";
-import NodeMap from "./NodeMap";
-import NodePreview from "./NodePreview";
 
 
 function App() {
-
-
-    const nodes = useNodes()
 
 
 
     return (
     <div className="App">
 
-        <NodeMap nodes={nodes}/>
-        <NodePreview nodes={nodes}/>
+        TEST
 
     </div>
     );
